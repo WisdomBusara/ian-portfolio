3224dccaeaa0568137f29550658acec2022bf615
Ian Kimani <ianmwaura@gmail.com>
2025-07-11 17:23:01 +0300

    0003

--- ian-portfolio/src/components/ThemeProviders.tsx
+++ ian-portfolio/src/components/ThemeProviders.tsx
@@ -1,8 +1,18 @@
 'use client';
-
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export function ThemeProviders({ children }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       {children}
